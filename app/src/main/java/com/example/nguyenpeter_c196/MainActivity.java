096f6be74d9252288e561a13a8759aed4c472f6e
peternguyen <peterdsd1@yahoo.com>
2023-02-01 11:09:41 -0800

    TermsListActivity and layout_terms_list.xml added. Add ability to change activity from main to terms list

--- app/src/main/java/com/example/nguyenpeter_c196/MainActivity.java
+++ app/src/main/java/com/example/nguyenpeter_c196/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.nguyenpeter_c196;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Application Home");
+    }
 
+    public void launchTerms(View v) {
+        //launch terms list
+        Intent i = new Intent(this, TermsListActivity.class);
+        startActivity(i);
     }
 
 }
---
+++ app/src/main/java/com/example/nguyenpeter_c196/TermsListActivity.java
@@ -0,0 +1,17 @@
+package com.example.nguyenpeter_c196;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class TermsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_list);
+
+    }
+
+}
