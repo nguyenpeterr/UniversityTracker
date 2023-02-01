32d5b456f19d94994e8881176c5b78d9cb5c724e
peternguyen <peterdsd1@yahoo.com>
2023-01-31 22:30:58 -0800

    Change launcher icon

--- app/src/main/java/com/example/nguyenpeter_c196/MainActivity.java
+++ app/src/main/java/com/example/nguyenpeter_c196/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.nguyenpeter_c196;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
