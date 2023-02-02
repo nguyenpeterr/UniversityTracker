0a40a514eeffb4aec7616968aa84bfda3683abef
peternguyen <peterdsd1@yahoo.com>
2023-02-02 14:42:00 -0800

    Added assessment activity class and xml layout page to add/edit assessments. Added recycler view to show assessments attached to course editor

---
+++ app/src/main/java/com/example/nguyenpeter_c196/AssessmentsActivity.java
@@ -0,0 +1,17 @@
+package com.example.nguyenpeter_c196;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AssessmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_assessment);
+
+    }
+
+}
--- app/src/main/java/com/example/nguyenpeter_c196/AssessmentsListActivity.java
+++ app/src/main/java/com/example/nguyenpeter_c196/AssessmentsListActivity.java
@@ -1,6 +1,8 @@
 package com.example.nguyenpeter_c196;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,11 @@ public class AssessmentsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessments_list);
 
+    }
+
+    public void faAddAssessment(View v) {
+        Intent i = new Intent(this, AssessmentsActivity.class);
+        startActivity(i);
     }
 
 }
